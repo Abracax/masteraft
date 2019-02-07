bf784fc33fb1ce38229cdc902b5054ee9433abb1
Zhenyun Yu <me@yuzhenyun.me>
2019-02-07 22:32:30 +0800

    finish raft state and fix

--- worker/src/raft_state.hpp
+++ worker/src/raft_state.hpp
@@ -1,15 +1,43 @@
 #ifndef MASTER_RAFT_RAFT_STATE_HPP
 #define MASTER_RAFT_RAFT_STATE_HPP
 
+#include <memory>
+#include <atomic>
+#include "common.hpp"
+
 namespace mr
 {
 
 class RaftState final
 {
 public:
-  RaftState() {}
+  RaftState()
+    : _role({Role.Candidate})
+    , _term({0})
+  {}
+
+public:
+  Role getRole() const {
+    return _role;
+  }
+
+  void setRole(Role) {
+    _role = role;
+  }
+
+  uint64_t getTerm() const {
+    return _term;
+  }
+
+  void setTerm(uint64_t term) {
+    _term = term;
+  }
 
 private:
+  std::atomic<Role> _role;
+  std::atomic_uint64_t _term;
+};
+
 }
 
 #endif
