9478974ff134bad81095d8643791436ff2f0967c
Zhenyun Yu <me@yuzhenyun.me>
2019-02-07 23:18:46 +0800

    added random generator part

--- worker/src/common.hpp
+++ worker/src/common.hpp
@@ -4,6 +4,11 @@
 #define MR_LOG std::clog
 #define MR_EOL std::endl
 
+// election term timeout
+// in MS
+#define MR_ELECT_LOW_BOUND 300
+#define MR_ELECT_HIGH_BOUND 500
+
 namespace mr
 {
 
--- worker/src/raft_server.hpp
+++ worker/src/raft_server.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <memory>
+#include <random>
 #include <boost/asio.hpp>
 #include "common.hpp"
 #include "http_status_server.hpp"
@@ -21,7 +22,13 @@ public:
   {
     boost::asio::ip::tcp::endpoint httpEp(boost::asio::ip::tcp::v4(), statusPort);
     _httpStatusServer = std::make_unique<HttpStatusServer>(_ctx, httpEp);
-    MR_LOG << "constructing RaftServer." << MR_EOL;
+
+    std::random_device rd;
+    _gen.reset(new std::mt19937{rd()});
+    _dist.reset(new std::uniform_int_distribution<>{
+	MR_ELECT_LOW_BOUND, MR_ELECT_HIGH_BOUND});
+    
+    MR_LOG << "RaftServer constructed." << MR_EOL;
   }
 
   RaftServer(const RaftServer &) = delete;
@@ -31,6 +38,7 @@ public:
   {
     MR_LOG << "start RaftServer invoked." << MR_EOL;
     _httpStatusServer->start();
+    setTimer();
     _ctx.run();
   }
   
@@ -38,11 +46,19 @@ public:
   {}
 
 protected:
+  void setTimer() {
+    uint32_t time = (*_dist)(*_gen);
+    MR_LOG << "timer time: " << time << MR_EOL;
+  }
+  
+protected:
   uint16_t _rpcPort;
   uint16_t _statusPort;
   boost::asio::io_context _ctx;
   std::unique_ptr<HttpStatusServer> _httpStatusServer;
   std::unique_ptr<RaftState> _state;
+  std::unique_ptr<std::mt19937> _gen;
+  std::unique_ptr<std::uniform_int_distribution<>> _dist;
 };
   
 }
